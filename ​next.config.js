bd0b20e510ea10770f71fa1e5def1bc0aa3befc8
Imanshu N <n4062226@gmail.com>
2026-06-28 13:27:14 +0530

    Create ​next.config.js

---
+++ ​next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: ['images.unsplash.com'], // 必要に応じて追加
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/models/',
+          outputPath: 'static/models/',
+        },
+      },
+    });
+
+    return config;
+  },
+};
+
+module.exports = nextConfig;
